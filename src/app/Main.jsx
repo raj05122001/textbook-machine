ae03739cff8b51a1e8f5e0aba8613758afeb4a3f
unknown <kushwaharohit967@gmail.com>
2025-10-29 12:33:52 +0530

    side bar id select

--- src/app/Main.jsx
+++ src/app/Main.jsx
@@ -5,40 +5,38 @@ import Header from "@/components/layout/Header";
 import Sidebar from "@/components/layout/Sidebar";
 import { Suspense } from "react";
 import 'katex/dist/katex.min.css';
+import { usePathname } from "next/navigation";
 
-const HEADER_H = 64; // match your Header height (px)
+const HEADER_H = 64;
 
 const Main = ({ children }) => {
+
+  const pathname = usePathname();
+
+  const segs = pathname.split("/").filter(Boolean);
+  const hideSidebar = segs[0] === "books" && segs.length === 2;
   return (
-    // lock layout to viewport; prevent page-level scroll
     <Suspense>
-    <div className="flex flex-col h-screen overflow-hidden">
-      {/* Header */}
-      <div style={{ height: HEADER_H }}>
-        <Header />
-      </div>
+      <div className="flex flex-col h-screen overflow-hidden">
+        <div style={{ height: HEADER_H }}>
+          <Header />
+        </div>
 
-      {/* Row: sidebar + content */}
-      <div className="flex flex-1 min-h-0">
-        <aside
-          className="hidden lg:flex lg:w-64 shrink-0 border-r border-gray-200 bg-white sticky thin-scrollbar"
-          // style={{
-          //   top: HEADER_H,
-          //   height: `calc(100vh - ${HEADER_H}px)`,
-          //   overflowY: "auto",
-          // }}
-        >
-          <Sidebar />
-        </aside>
+        <div className="flex flex-1 min-h-0">
+          {!hideSidebar && (
+            <aside className="hidden lg:flex lg:w-64 shrink-0 border-r border-gray-200 bg-white sticky thin-scrollbar">
+              <Sidebar />
+            </aside>
+          )}
 
-        <main
-          className="flex-1 bg-gray-50 overflow-y-auto min-h-0"
-          style={{ height: `calc(100vh - ${HEADER_H}px)` }}
-        >
-          <div className="p-4">{children}</div>
-        </main>
+          <main
+            className="flex-1 bg-gray-50 overflow-y-auto min-h-0"
+            style={{ height: `calc(100vh - ${HEADER_H}px)` }}
+          >
+            <div className="p-4">{children}</div>
+          </main>
+        </div>
       </div>
-    </div>
     </Suspense>
   );
 };
